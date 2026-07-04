fed14d08dc281feb6d0878e0b29c918ee8fb9d29
Mudwin <nshmilsson@gmail.com>
2026-07-04 18:13:04 +0500

    add adaptive layout for phones

--- src/components/NavButton/NavButton.tsx
+++ src/components/NavButton/NavButton.tsx
@@ -18,9 +18,15 @@ const NavButton = ({
   onClick,
 }: NavButtonProps) => {
   return (
-    <button className={styles.button} onClick={onClick}>
+    <button
+      className={`${styles.button} ${state === "open" ? styles.active : ""}`}
+      onClick={onClick}
+    >
       {state === "open" ? (
-        <span>{children}</span>
+        <>
+          <span>{children}</span>
+          <img className={styles.disabledOnPC} src={iconSrc} alt={page} />
+        </>
       ) : (
         <img src={iconSrc} alt={page} />
       )}
